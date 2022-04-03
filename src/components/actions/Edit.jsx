2ec8523d67f045a8ca51f4940527196ddc4fad50
Andrei shitikov <andrei_shitikov@epam.com>
2022-04-03 08:41:05 +0200

    Add placeholder for actions edit field

--- src/components/actions/Edit.jsx
+++ src/components/actions/Edit.jsx
@@ -1,75 +1,68 @@
-import {
-  Button,
-  Grid,
-  TextField,
-} from "@mui/material";
+import { Button, Grid, TextField } from "@mui/material";
 import React from "react";
 
 const Edit = ({ action, onEdit, switchView }) => {
   const { name, onHit, type, reach, target, description } = action;
   const onChange = ({ name, value }) => onEdit({ ...action, [name]: value });
+  const sharedInputProps = {
+    variant: "outlined",
+    size: "small",
+    onChange: (event) => onChange(event.target),
+  };
   return (
     <Grid item container justifyContent="center" spacing={2} marginTop={2}>
       <Grid item xs={12}>
         <TextField
           label="Name"
-          variant="outlined"
-          size="small"
           name="name"
           value={name}
-          onChange={(event) => onChange(event.target)}
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={6}>
         <TextField
           label="Type"
-          variant="outlined"
-          size="small"
-          name="type"
           value={type}
-          onChange={(event) => onChange(event.target)}
+          name="type"
+          placeholder="Mele"
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={6}>
         <TextField
           label="Reach"
-          variant="outlined"
-          size="small"
           name="reach"
           value={reach}
-          onChange={(event) => onChange(event.target)}
+          placeholder ="15 ft."
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={6}>
         <TextField
           label="On heat"
-          variant="outlined"
-          size="small"
           name="onHit"
           value={onHit}
-          onChange={(event) => onChange(event.target)}
+          placeholder = "10"
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={6}>
         <TextField
           label="Target"
-          variant="outlined"
-          size="small"
           name="target"
           value={target}
-          onChange={(event) => onChange(event.target)}
+          placeholder = "one creature"
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={12}>
         <TextField
           label="Description"
-          variant="outlined"
-          size="small"
           name="description"
           multiline
           fullWidth
           value={description}
-          onChange={(event) => onChange(event.target)}
+          {...sharedInputProps}
         />
       </Grid>
       <Grid item xs={1}>
